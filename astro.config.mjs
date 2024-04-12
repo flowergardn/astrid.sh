faff9305f2ced349a1a10ee923aabd6c6b9efb92
astridlol <me@astrid.sh>
2024-04-12 12:41:05 -0400

    feat: add silly music section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [react(), tailwind(), mdx()],
+  output: "hybrid",
+  adapter: vercel(),
 });
