4b8fb6f49b89af8a6510984de2097b0999ee60f1
Astrid <45674371+astridlol@users.noreply.github.com>
2024-04-13 17:02:55 -0400

    Feat/music (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
+  output: "server",
+  adapter: vercel(),
 });
