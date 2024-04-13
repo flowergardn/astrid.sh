2231ac3ea12d58bc768156e67d2b42bbf4a6e9ff
astridlol <me@astrid.sh>
2024-04-13 16:23:43 -0400

    feat: fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
