b01bc8bf01763b04526339e911ba408c8a053f3a
flowergardn <me@astrid.sh>
2024-12-04 17:33:48 -0500

    chore: fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind({
-        applyBaseStyles: false,
-    }),]
+  integrations: [react(), tailwind({
+      applyBaseStyles: false,
+  }),],
+
+  output: "server",
+  adapter: vercel(),
 });
