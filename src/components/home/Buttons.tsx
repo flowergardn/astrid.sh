65bd2e48ee0248bf429f46097ae62d6a4a14dfec
astrid <astrid@astrids-MacBook-Air.local>
2026-07-29 07:08:40 -0400

    feat: add rough descriptions

--- src/components/home/Buttons.tsx
+++ src/components/home/Buttons.tsx
@@ -15,35 +15,51 @@ const buttons: Button[] = [
     description: "you are here!",
   },
   {
+    name: "lumie",
     imageURL: "/buttons/lumie-badge.png",
+    description:
+      "cool british girl & femtanyl enjoyer - she's also a gamedev student!",
   },
   {
     siteURL: "https://aroze.me/",
     imageURL: "/buttons/aroze-badge.png",
+    description:
+      "one of the people i've known for the longest time, she's my best friend and you should check out her stuff.",
   },
   {
     siteURL: "https://lily.pet/",
     imageURL: "/buttons/lily-badge.gif",
+    description:
+      "lily is my favorite blahaj haver, i love every one of our conversations",
   },
   {
     siteURL: "https://eva.ac/",
     imageURL: "/buttons/eva-badge.png",
+    description:
+      "eva is most known for getting up to shenanigans, but she's genuinely an amazing friend too",
   },
   {
     siteURL: "https://jos.gg/",
     imageURL: "/buttons/jos-badge.gif",
+    description:
+      "jos is a great guy that is literally everywhere, he's super smart & also a fellow cookies enjoyer that has a cute cat",
   },
   {
     siteURL: "https://jamie.rs/",
     imageURL: "/buttons/jamie-badge.png",
+    description: "me & jamie don't talk much anymore, but she's still cool 🤍",
   },
   {
     siteURL: "https://santio.me/",
     imageURL: "/buttons/santio-badge.webp",
+    description:
+      "you know how they say, if you're the smartest in the room, leave the room? santio's knowledge in software development is 100x mine.",
   },
   {
     siteURL: "https://ollie.lol/",
     imageURL: "/buttons/ollie-badge.png",
+    description:
+      "Ollie is solely responsible for nearly every friend i have, thanks to starting a Minecraft SMP in 2022 <3",
   },
 ];
 
@@ -55,7 +71,7 @@ function DisplayBtnInfo({ btn }: { btn: Button }) {
   }
 
   return (
-    <div className="w-[440px] min-h-36 border border-border bg-background/50 p-4">
+    <div className="w-[440px] min-h-36 border border-border border-pink bg-background/50 p-4">
       <div className="flex items-center gap-3">
         <img src={btn.imageURL} alt={title} />
         <h3>{title}</h3>
